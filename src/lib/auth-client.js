d7e73378a7184bf7e3a060c86feb60e6d36a884b
musazahidtamim3-ai <musazahidtamim3@gmail.com>
2026-06-29 20:09:40 +0300

    try to deploy fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-     baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+     baseURL: process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000",
 })
 
 export const { signIn, signUp, useSession, signOut, updateUser } = authClient;
