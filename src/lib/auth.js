3e4aea6e3561b5fbabcf68f5dce273c215aac79f
musazahidtamim3-ai <musazahidtamim3@gmail.com>
2026-06-30 08:52:16 +0300

    login issue fixed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -18,6 +18,11 @@ export const auth = betterAuth({
      baseURL: process.env.BETTER_AUTH_URL,
      secret: process.env.BETTER_AUTH_SECRET,
 
+     trustedOrigins: [
+          "http://localhost:3000",
+          "https://daily-life-client.vercel.app"
+     ],
+
      emailAndPassword: {
           enabled: true,
      },
